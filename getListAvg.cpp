fe352999d6efed553b0cb533754617c215b50dd8
Hyman Ho <122756544+hymanho@users.noreply.github.com>
2025-04-23 23:58:08 -0700

    Update getListAvg.cpp

--- getListAvg.cpp
+++ getListAvg.cpp
@@ -12,17 +12,19 @@ highest possible average? Int_max - 1, as no value can go beyond this.
 lowest possible average? INT_MIN. no value can go below this
 
 if every value in the list is within this range, the avg will never be above INT_MAX-1,
-and below INT_MIN-1
+and below INT_MIN
 
-that means a variable that keeps track of average has no chance of overflowing.
-IF WE recalculate the AVERAGE after every iteration until we reach the sentinel
+that means a variable that keeps track of average has no chance of overflowing, if
+we recalculate the AVERAGE after every iteration until we reach the sentinel value (INT_MAX)
 
-tldr: use a running average strategy to solve overflow and underflow of sum, discarding
+tldr: use a running average strategy to solve issue of overflow and underflow with a sum, discarding
 the use of a sum variable 
 
-to solve overflow on counter, we can set it to be a double. double's do not overflow
+to solve overflow on counter, we can set it to be a double. double's do not overflow or underflow
 like integers, and instead result in a loss of precision. this loss of precision is
-extremely minute, to the point if negligiblity. 
+extremely minute, to the point if negligiblity, and does not cause the program to crash. 
+
+ 
 
 for example, if we had 2^53 1's in a list, and then the 2^53 + 1 value is INT_MAX - 1.
 Even though the counter value loses precision, the overall average would still be 1,
